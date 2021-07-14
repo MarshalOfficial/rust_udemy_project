6f379a8b3c4c0306117b8067ca4b4ebdca6e0caa
MohammadReza <mr.rahmatzade371@gmail.com>
2021-07-14 22:53:11 +0430

    feat core datatypes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::mem;
+
 fn main() {
-    println!("Hello, world!");
+    // unsigned 0 +
+    let a: u8 = 123; //8bit
+
+    // signed
+    let c: i16 = 555; //8bit
+
+    println!("a = {}", a);
+    println!("c = {}", c);
+
+    //mut that value can change
+    let mut b: i8 = 0;
+    println!("b = {}", b);
+    b = 42;
+    println!("b = {}", b);
+
+    let mut d = 123456789; // 32 bit signed i32
+    println!("c = {}, size = {} bytes", d, mem::size_of_val(&d));
+    d = -1;
+    println!(
+        "c = {}, size = {} bytes, after modification.",
+        d,
+        mem::size_of_val(&d)
+    );
+
+    let z: isize = 123; //take the maximum size base on the operation system x86 or x64
+    let size_of_z = mem::size_of_val(&z);
+    println!(
+        "z = {}, takes up {} bytes, {}-bit os",
+        z,
+        size_of_z,
+        size_of_z * 8
+    );
+
+    let f = 'x';
+    println!("f = {}, size = {} bytes", f, mem::size_of_val(&f));
+
+    let e = 2.5;
+    let g: f32 = 2.5;
+    println!("e = {}, size = {} bytes", e, mem::size_of_val(&e));
+    println!("g = {}, size = {} bytes", g, mem::size_of_val(&g));
+
+    //true false
+    let j = false;
+    println!("j = {}, size = {} bytes", j, mem::size_of_val(&j));
 }
