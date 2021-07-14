505ffa249b4a215250fecf5a6750a0ef872292de
MohammadReza <mr.rahmatzade371@gmail.com>
2021-07-14 23:16:14 +0430

    feat scope and shadowing

--- src/main.rs
+++ src/main.rs
@@ -26,54 +26,73 @@ fn operators() {
     let ss = std::f64::consts::PI < 4.0; //true // > <= == >=
     println!("ss = {}",ss);
 
+
+}
+
+fn fundamental_data_types(){
+    // unsigned 0 +
+    let a: u8 = 123; //8bit
+
+    // signed
+    let c: i16 = 555; //8bit
+
+    println!("a = {}", a);
+    println!("c = {}", c);
+
+    //mut that value can change
+    let mut b: i8 = 0;
+    println!("b = {}", b);
+    b = 42;
+    println!("b = {}", b);
+
+    let mut d = 123456789; // 32 bit signed i32
+    println!("c = {}, size = {} bytes", d, mem::size_of_val(&d));
+    d = -1;
+    println!(
+        "c = {}, size = {} bytes, after modification.",
+        d,
+        mem::size_of_val(&d)
+    );
+
+    let z: isize = 123; //take the maximum size base on the operation system x86 or x64
+    let size_of_z = mem::size_of_val(&z);
+    println!(
+        "z = {}, takes up {} bytes, {}-bit os",
+        z,
+        size_of_z,
+        size_of_z * 8
+    );
+
+    let f = 'x';
+    println!("f = {}, size = {} bytes", f, mem::size_of_val(&f));
+
+    let e = 2.5;
+    let g: f32 = 2.5;
+    println!("e = {}, size = {} bytes", e, mem::size_of_val(&e));
+    println!("g = {}, size = {} bytes", g, mem::size_of_val(&g));
+
+    //true false
+    let j = false;
+    println!("j = {}, size = {} bytes", j, mem::size_of_val(&j));
+}
+
+fn scope_and_shadowing(){
+    let a = 123;
     
+    {
+        let b = 456;
+        println!("inside b = {}",b);
+        let a = 777; //it shadows outer a
+        println!("inside a = {}",a);    
+    }
+    
+    println!("outside a = {}",a);
+    //println!("outside b = {}",b); //illegal
+
 }
 
 fn main() {
-    operators();
-
-    // // unsigned 0 +
-    // let a: u8 = 123; //8bit
-
-    // // signed
-    // let c: i16 = 555; //8bit
-
-    // println!("a = {}", a);
-    // println!("c = {}", c);
-
-    // //mut that value can change
-    // let mut b: i8 = 0;
-    // println!("b = {}", b);
-    // b = 42;
-    // println!("b = {}", b);
-
-    // let mut d = 123456789; // 32 bit signed i32
-    // println!("c = {}, size = {} bytes", d, mem::size_of_val(&d));
-    // d = -1;
-    // println!(
-    //     "c = {}, size = {} bytes, after modification.",
-    //     d,
-    //     mem::size_of_val(&d)
-    // );
-
-    // let z: isize = 123; //take the maximum size base on the operation system x86 or x64
-    // let size_of_z = mem::size_of_val(&z);
-    // println!(
-    //     "z = {}, takes up {} bytes, {}-bit os",
-    //     z,
-    //     size_of_z,
-    //     size_of_z * 8
-    // );
-
-    // let f = 'x';
-    // println!("f = {}, size = {} bytes", f, mem::size_of_val(&f));
-
-    // let e = 2.5;
-    // let g: f32 = 2.5;
-    // println!("e = {}, size = {} bytes", e, mem::size_of_val(&e));
-    // println!("g = {}, size = {} bytes", g, mem::size_of_val(&g));
-
-    // //true false
-    // let j = false;
-    // println!("j = {}, size = {} bytes", j, mem::size_of_val(&j));
+    //fundamental_data_types();
+    //operators();
+    scope_and_shadowing();    
 }
