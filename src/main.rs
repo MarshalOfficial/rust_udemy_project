eb8833f8e4388ea52165b7f1acc65da1e94238a4
MohammadReza <mr.rahmatzade371@gmail.com>
2021-07-15 15:24:28 +0430

    feat functions

--- src/main.rs
+++ src/main.rs
@@ -432,8 +432,26 @@ fn generics() {
     //println!("{:?}",myline);
 }
 
+fn increase(x: &mut i32){
+    *x +=1;
+}
+fn functions(){
+    let mut z = 1;
+    increase(&mut z);
+    println!("z = {}",z);
+    let a = 3;
+    let b = 5;
+    let p = product(a,b);
+    println!("p={}",p);
+}
+
+fn product(x:i32,y:i32) -> i32{
+    x * y
+}
+
 fn main() {
-    generics();
+    functions();
+    //generics();
     //pm::pattern_matching();
     //tuples();
     //strings();
