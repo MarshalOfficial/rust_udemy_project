c40b10a7b9ec708f24fa30930e280b174960409a
MohammadReza <mr.rahmatzade371@gmail.com>
2021-07-19 23:06:33 +0430

    feat mutex

--- src/main.rs
+++ src/main.rs
@@ -702,31 +702,40 @@ fn rc_demo() {
     println!("Name = {}", name);
 }
 
+use std::sync::{Arc, Mutex};
 use std::thread;
-use std::sync::Arc;
 
 struct Personn2 {
     name: Arc<String>,
+    state: Arc<Mutex<String>>,
 }
 
 impl Personn2 {
-    fn new(name: Arc<String>) -> Personn2 {
-        Personn2 { name: name }
+    fn new(name: Arc<String>, state: Arc<Mutex<String>>) -> Personn2 {
+        Personn2 {
+            name: name,
+            state: state,
+        }
     }
 
     fn greet(&self) {
-        println!("Hi, my name is {}", self.name);
+        let mut state = self.state.lock().unwrap();
+        state.clear();
+        state.push_str("excited");
+
+        println!("Hi, my name is {} and I am {}", self.name, state.as_str());
     }
 }
 
 fn arc_demo() {
     let name = Arc::new("John".to_string());
-    let person = Personn2::new(name.clone());
+    let state = Arc::new(Mutex::new("bored".to_string()));
+    let person = Personn2::new(name.clone(), state.clone());
 
     let t = thread::spawn(move || {
         person.greet();
     });
-    println!("Name = {}", name);
+    println!("Name = {}, state = {}", name, state.lock().unwrap().as_str());
     t.join().unwrap();
 }
 
